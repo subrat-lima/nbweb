7f7ac53c3839ae39aea10656321c859317efe79a
subrat-lima <subrat.k.lima@subrat.k.lima@protonmail.com>
2024-09-06 13:53:17 +0530

    fix import error in cli

--- src/nbweb/cli.py
+++ src/nbweb/cli.py
@@ -1,14 +1,13 @@
 import argparse
 
 from nbweb import __VERSION__
-from nbweb.app import check_is_supported, get_content, get_rss
+from nbweb.app import check_is_supported, get_content
 
 
 def parse_args():
     parser = argparse.ArgumentParser(
         prog="nbweb",
-        description="extracts bloat free content",
-        epilog="takes in an url and returns clean content",
+        description= "nbweb is a simple tool to extract data from websites",
     )
     parser.add_argument("url", nargs="?", type=str, help="enter an url")
     parser.add_argument(
@@ -23,9 +22,6 @@ def parse_args():
         "--supported", action="store_true", help="returns yes if supported else no"
     )
 
-    parser.add_argument(
-        "--rss", action="store_true", help="returns the rss information"
-    )
 
     parser.add_argument(
         "-v", "--version", action="store_true", help="show version number"
@@ -38,9 +34,7 @@ def parse_args():
 def cli() -> None:
     args = parse_args()
 
-    if args.rss:
-        content = get_rss(args.url)
-    elif args.supported:
+    if args.supported:
         content = check_is_supported(args.url)
     elif args.version:
         content = __VERSION__
