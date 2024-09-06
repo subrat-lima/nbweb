3330182d58dbf5aa817440b0837d8593b268835f
subrat-lima <subrat.k.lima@subrat.k.lima@protonmail.com>
2024-09-06 15:03:54 +0530

    [ie/techspot] added fix

--- src/nbweb/extractor/techspot.py
+++ src/nbweb/extractor/techspot.py
@@ -3,13 +3,19 @@ from .common import InfoExtractor
 
 class TechSpotIE(InfoExtractor):
     _IE_NAME = "techspot"
-    _VALID_URLS = [r"https://www.techspot.com/news/(?P<id>[\w-]+)"]
-    _TESTS = []
+    _VALID_URLS = [r"https://www.techspot.com/news/(?P<id>[\d]+)[\w-]+"]
+    _TESTS = [{
+            "url": "https://www.techspot.com/news/104596-lenovo-auto-twist-pc-has-display-automatically-follows.html",
+            "info_dict": {
+                "id": "104596",
+                "title": "Lenovo's crazy \"Auto Twist\" PC can turn and face you automatically",
+            },
+        }]
 
     def _extract(self, url):
-        id = self.get_id(url)
+        id = self._get_id(url)
         webpage = self._request_webpage(url)
-        title = self._query_selector(webpage, "h1::text")
+        title = self._get_meta_property(webpage, "title")
         content = self._query_selector(
             webpage,
             "div.articleBody > :not(div.subDriveRevBot):not(.twitter-tweet):not(div.related-news):not(style) *::text",
